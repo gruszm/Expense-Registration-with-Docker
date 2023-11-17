5aac5ab20d613c5984c2f8af854377a82380c4c4
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-17 18:21:03 +0100

    Adjusted to the container

--- src/main/java/com/gruszm/kontenery/projekt_konteneryzacja/ProjektKonteneryzacjaApplication.java
+++ src/main/java/com/gruszm/kontenery/projekt_konteneryzacja/ProjektKonteneryzacjaApplication.java
@@ -1,7 +1,13 @@
 package com.gruszm.kontenery.projekt_konteneryzacja;
 
+import com.gruszm.kontenery.projekt_konteneryzacja.entities.Book;
+import com.gruszm.kontenery.projekt_konteneryzacja.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ProjektKonteneryzacjaApplication {
@@ -10,4 +16,15 @@ public class ProjektKonteneryzacjaApplication {
 		SpringApplication.run(ProjektKonteneryzacjaApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner cmd(BookRepository bookRepository)
+	{
+		return runner ->
+		{
+			List<Book> books = bookRepository.findAll();
+
+			System.out.println("------------Books in the database------------");
+			books.forEach(book -> System.out.println(book));
+		};
+	}
 }
--- src/main/java/com/gruszm/kontenery/projekt_konteneryzacja/entities/Book.java
+++ src/main/java/com/gruszm/kontenery/projekt_konteneryzacja/entities/Book.java
@@ -52,4 +52,14 @@ public class Book {
     {
         this.author = author;
     }
+
+    @Override
+    public String toString()
+    {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
