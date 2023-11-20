996156b023b81316e056b380ad9dffeb2e15d9e1
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-20 20:10:23 +0100

    Role deleted

--- src/main/java/com/gruszm/kontenery/projekt_konteneryzacja/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gruszm.kontenery.projekt_konteneryzacja.repositories;
-
-import com.gruszm.kontenery.projekt_konteneryzacja.entities.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String>
-{
-
-}
