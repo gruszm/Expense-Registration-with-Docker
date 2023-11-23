02f839302ad53c6197f0884d251ba5af5aba1b10
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-23 23:51:01 +0100

    Adding a new category form

--- my_app_front/src/main/java/com/gruszm/kontenery/controllers/CategoryController.java
+++ my_app_front/src/main/java/com/gruszm/kontenery/controllers/CategoryController.java
@@ -2,11 +2,13 @@ package com.gruszm.kontenery.controllers;
 
 import com.gruszm.kontenery.entities.Category;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -31,5 +33,30 @@ public class CategoryController
         List<Category> categories = Arrays.asList(categoriesResponse.getBody());
         model.addAttribute("categories", categories);
         return "categories/show-categories";
+    }
+
+    @GetMapping("/categoryForm")
+    public String getCategoryForm(Model model)
+    {
+        Category category = new Category();
+        model.addAttribute("category", category);
+        return "categories/category-form";
+    }
+
+    @PostMapping("/processCategoryForm")
+    public String processCategoryForm(@ModelAttribute(name = "category") Category category)
+    {
+        String url = "http://" + host + ":" + port + "/api/categories";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Category> requestEntity = new HttpEntity<>(category, headers);
+        ResponseEntity<Category> categoryResponse = restTemplate.postForEntity(url, requestEntity, Category.class);
+
+        System.out.println(categoryResponse.getStatusCode());
+
+        return "redirect:/categories";
     }
 }
