c68b4d49d395fc0c96d04bda3bc42bc122d23a5e
gruszm <mariuszgruszczynski11@gmail.com>
2023-11-29 19:24:52 +0100

    External api: cat facts

--- my_app_front/src/main/java/com/gruszm/kontenery/controllers/HomeController.java
+++ my_app_front/src/main/java/com/gruszm/kontenery/controllers/HomeController.java
@@ -1,8 +1,11 @@
 package com.gruszm.kontenery.controllers;
 
+import com.gruszm.kontenery.externalapi.CatFact;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HomeController
@@ -10,6 +13,11 @@ public class HomeController
     @GetMapping
     public String getHome(Model model)
     {
+        String url = "https://catfact.ninja/fact";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<CatFact> catFactResponse = restTemplate.getForEntity(url, CatFact.class);
+        model.addAttribute("catfact", catFactResponse.getBody());
+
         return "home";
     }
 }
